e270545b9d00ab618b00c9ab934e701a84f4b322
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2024-08-04 01:22:52 +0530

    followingFeed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,28 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
 import prisma from "@/lib/prisma";
-import TrendsSidebar from '../../components/TrendsSidebar';
+import TrendsSidebar from "../../components/TrendsSidebar";
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
-export default  function Home() {
-
-
+export default function Home() {
   return (
     <main className="flex w-full gap-5">
-      <div className="flex w-full flex-col gap-5 lg:w-2/3">
+      <div className="flex w-full flex-col gap-5  lg:w-2/3">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For You</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
