b639d783dc0c8187d0fa2453549689d04efefbb2
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2024-10-30 15:19:07 +0530

    notification ui

--- src/app/(main)/notifications/page.tsx
+++ src/app/(main)/notifications/page.tsx
@@ -12,11 +12,11 @@ export default function Page() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
-        <div className="rounded-2xl border-b p-5 shadow-sm">
+        <div className="rounded-2xl py-5 px-3 shadow-sm flex ">
           <Link href={`/`}>
             <ArrowLeft />
           </Link>
-          <h1 className="text-center text-2xl font-bold">Notifications</h1>
+          <h1 className="text-center text-2xl font-bold mx-auto">Notifications</h1>
         </div>
         <Notifications />
       </div>
